9a050dd04bcece6c4796d88e059ea341deae8e8b
Gachenge <gachenge1@gmail.com>
2023-07-18 01:05:48 -0700

    corrections for the output

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -10,7 +10,7 @@ def main(mongo_collection):
     method_stats = [collection.count_documents({'method': method}) for method in methods]
     print("Methods:")
     for method, stat in zip(methods, method_stats):
-        print(f"method {method}: {stat}")
+        print(f"\tmethod {method}: {stat}")
     status_logs = collection.count_documents({'method': 'GET', 'path': '/status'})
     print(f"{status_logs} status check")
 
